f1a84147c8e0dcb9dc821e07f12a6a7dcb179a56
Axel Rikte <axelrikte@Axels-MacBook-Pro.local>
2018-08-15 13:20:12 +0200

    More functionality added

--- script.js
+++ script.js
@@ -9,14 +9,23 @@ document.addEventListener("DOMContentLoaded",
 		var yCount = 0;
 		var content = document.getElementById("content");
 		var poäng = 0;
-		var design = document.getElementById("jSDesign");
+		var design = document.getElementById("jsDesign");
 		var variabeln;
 		var ordning = 1;
 		var stoppa = 0;
 		var bitTyp = 0;
 		var mitt = Math.round(horisontell/2);
 		var klossPosition;
-
+		var horisontellPlacering = Math.round(horisontell/2); // Håller reda på klossens vertikala placering så att den vet när man kan snurra så att inte en bit av klossen sticker ut på andra sidan.
+		var vertikalPlacering = 1;  // som horisontellPlacering fast i vertikalled.
+		var stickUtHöger;
+		var stickUtVänster;
+		var stickUtUpp;
+		var tryckNer;
+		var tryckHöger;
+		var frånVänsterKant;
+		var frånHögerKant;
+		var frånTopp;
 		// Betydelse av listvärden för divar. 0: inget på platsen 1: aktiv 2: blev stuck denna "tick" 3: stuck 
 		// + (horisontell*(50/andel)) +
 		function styleSetup () {
@@ -64,27 +73,10 @@ document.addEventListener("DOMContentLoaded",
 			console.log("skapaLista");
 		};
 
-		function testaOmRadKlar () {	//testar om den understa raden är fylld KAN VARA BUGGIG!!
-			var steg = 0;
-			var antalFyllda = 0;
-			while (steg < horisontell) {
-				if (lista[vertikal*horisontell-steg] === 1) {
-					antalFyllda++;
-				};
-				steg++;
-			};
-			if (antalFyllda == horisontell) {     // antalFyllda=x?????????????
-				// document.getElementById("content").innerHTML = document.getElementById("content").innerHTML + "<div class='hugeGreen'> win </h1>";
-				console.log("Den sista raden har fyllts!!!");
-				stoppa++;
-				// return "radklar";
-			};
-			console.log("testaOmRadKlar");
-		};
-
 		function flyttaNerRad () {
 			var plats = 1;
 			var scroll = 1;
+			vertikalPlacering++;
 			var nyLista = new Array();
 			while (plats <= horisontell*vertikal) {
 				if (lista[plats] == 1) {
@@ -108,36 +100,73 @@ document.addEventListener("DOMContentLoaded",
 			var mög = 0;
 			var megamög = 0;
 			plats = 1;
-			while (mög < horisontell) {
-				if (lista[vertikal*horisontell-mög] == 3 || lista[vertikal*horisontell-mög] == 2) {
-					megamög++;
+			// while (mög < horisontell) {
+			// 	if (lista[vertikal*horisontell-mög] == 3 || lista[vertikal*horisontell-mög] == 2) {
+			// 		megamög++;
+			// 	};
+			// 	mög++;
+			// 	if (megamög == horisontell) {
+			// 		console.log("radfull!!")
+			// 		var mögLista = new Array();
+			// 		while (plats <= vertikal*horisontell) {
+			// 			if (lista[plats] == 3 || lista[plats] == 2) {
+			// 				scroll = lista[plats];
+			// 				mögLista[plats+horisontell] = scroll;
+			// 			}else if (lista[plats] == 1) {
+			// 				scroll = lista[plats];
+			// 				mögLista[plats] = scroll;
+			// 			};
+			// 			plats++;
+			// 		};
+			// 		plats = 1;
+			// 		while (plats <= vertikal*horisontell) {
+			// 			scroll = mögLista[plats];
+			// 			lista[plats] = scroll;
+			// 			plats++;
+			// 		};
+			// 		megamög = 0;
+			// 		mög = 0;
+			// 	};
+
+			// };
+			mitt = mitt + horisontell;
+			console.log("flyttaNerRad");
+		};
+
+		function kollaSistaRadenOchFlyttaNer () { // används inte men kanske behövs!!
+			var sistaRadKoll = 1;
+			var fylldaISistaRaden = 0;
+			var plats = 1;
+			var scroll;
+			var fyllFörstaRadenINyLista = 1;
+			while (sistaRadKoll <= horisontell) {
+				if (lista[vertikal*horisontell-sistaRadKoll] == 2 || lista[vertikal*horisontell-sistaRadKoll] == 3) {
+					fylldaISistaRaden++;
+					sistaRadKoll++;
+				}else {
+					sistaRadKoll++;
 				};
-				mög++;
-				if (megamög == horisontell) {
-					var mögLista = new Array();
-					while (plats <= vertikal*horisontell) {
-						if (lista[plats] == 3 || lista[plats] == 2) {
-							scroll = lista[plats];
-							mögLista[plats+horisontell] = scroll;
-						}else if (lista[plats] == 1) {
-							scroll = lista[plats];
-							mögLista[plats] = scroll;
-						};
+				if (fylldaISistaRaden == horisontell) {
+					sistaRadKoll = 1;
+					while (plats <= horisontell*vertikal-horisontell) {
+						scroll = lista[plats];
+						nyLista[plats+horisontell];
 						plats++;
+					};
+					while (fyllFörstaRadenINyLista <= horisontell) {
+						nyLista[fyllFörstaRadenINyLista] = 0;
+						fyllFörstaRadenINyLista++;
 					};
 					plats = 1;
 					while (plats <= vertikal*horisontell) {
-						scroll = mögLista[plats];
+						scroll = nyLista[plats];
 						lista[plats] = scroll;
 						plats++;
 					};
-					megamög = 0;
-					mög = 0;
+					kollaSistaRadenOchFlyttaNer();
 				};
-
 			};
-			mitt = mitt + horisontell;
-			console.log("flyttaNerRad");
+			console.log("kollaSistaRadenOchFlyttaNer")
 		};
 
 		function rita () {	//funktionen ritar rätt färg på rutan beroende på motsvarande divbox's värde i listan
@@ -184,36 +213,61 @@ document.addEventListener("DOMContentLoaded",
 			};
 			if (summa == 0) {
 				klossPosition = 1;
-				var mitt = Math.round(horisontell/2);
+				horisontellPlacering = Math.round(horisontell/2);
+				vertikalPlacering = 1;
+				mitt = Math.round(horisontell/2);
 				console.log("horisontell/2 ish: " + Math.round(horisontell/2))
 				var klossSlumpare = Math.round(Math.random()*100);
 				console.log(klossSlumpare);
-				if (0 <= klossSlumpare && klossSlumpare < 25) {
+				if (0 <= klossSlumpare && klossSlumpare < 14) {
 					lista[mitt] = 1;
-					lista[mitt+horisontell] = 1;
-					lista[mitt+2*horisontell] =1;
+					lista[mitt+1] = 1;
+					lista[mitt+2] = 1;
+					lista[mitt-1] = 1;
 					bitTyp = 1;
-					console.log("bit vertikal skapad");
-				}else if (25 < klossSlumpare && klossSlumpare < 50){
+					console.log("I");
+				}else if (14 < klossSlumpare && klossSlumpare < 28){
 					lista[mitt] = 1;
 					lista[mitt+1] = 1;
 					lista[mitt-1] = 1;
-					bitTyp = 2;
-					console.log("bit horisontell skapad");
-				}else if (50 < klossSlumpare && klossSlumpare < 75){
-					lista[mitt] = 1;
-					lista[mitt+1] = 1;
-					lista[mitt-1] = 1;
-					lista[mitt+1+horisontell] = 1;
-					bitTyp = 3;
-					console.log("krokbit2 skapad");
-				}else if (75 < klossSlumpare && klossSlumpare < 100){
-					lista[mitt] = 1;
-					lista[mitt+1] = 1;
 					lista[mitt-1+horisontell] = 1;
+					bitTyp = 2;
+					console.log("J");
+				}else if (28 < klossSlumpare && klossSlumpare < 42){
+					lista[mitt] = 1;
+					lista[mitt+1] = 1;
 					lista[mitt-1] = 1;
+					lista[mitt-1+horisontell] = 1;
+					bitTyp = 3;
+					console.log("L");
+				}else if (42 < klossSlumpare && klossSlumpare < 56){
+					lista[mitt] = 1;
+					lista[mitt+1] = 1;
+					lista[mitt+horisontell] = 1;
+					lista[mitt+1+horisontell] = 1;
 					bitTyp = 4;
-					console.log("krokbit skapad");
+					console.log("O");
+				}else if (56 < klossSlumpare && klossSlumpare < 70){
+					lista[mitt] = 1;
+					lista[mitt+1] = 1;
+					lista[mitt+horisontell] = 1;
+					lista[mitt-1+horisontell] = 1;
+					bitTyp = 5;
+					console.log("S");
+				}else if (70 < klossSlumpare && klossSlumpare < 84){
+					lista[mitt] = 1;
+					lista[mitt+1] = 1;
+					lista[mitt+horisontell] = 1;
+					lista[mitt-1] = 1;
+					bitTyp = 6;
+					console.log("T");
+				}else if (84 < klossSlumpare && klossSlumpare < 100){
+					lista[mitt] = 1;
+					lista[mitt-1] = 1;
+					lista[mitt+horisontell] = 1;
+					lista[mitt+1+horisontell] = 1;
+					bitTyp = 7;
+					console.log("Z");
 				};
 			};
 		};
@@ -231,11 +285,177 @@ document.addEventListener("DOMContentLoaded",
 			var blockerare = 0;
 			var nyLista = new Array();
 			if (event.key == "w" || event.key == "W") {
-				while (scroll <= horisontell*vertikal) {
-					if (lista[scroll] == 1) {
-						lista[scroll] = 0;
+				if (bitTyp == 1) {
+					if (klossPosition == 1) {
+						stickUtVänster = 0;
+						stickUtHöger = 0;
+						stickUtUpp = 1;
+					}else if (klossPosition == 2) {
+						stickUtVänster = 1;
+						stickUtHöger = 2;
+						stickUtUpp = 0;
+					}else if (klossPosition == 3) {
+						stickUtVänster = 0;
+						stickUtHöger = 0;
+						stickUtUpp = 1;
+					}else if (klossPosition == 4) {
+						stickUtVänster = 1;
+						stickUtHöger = 2;
+						stickUtUpp = 0;
 					};
-					scroll++;
+					testaOmPush();
+					if (klossPosition == 1) {
+						testaOmRotation(mitt, mitt+horisontell, mitt+horisontell*2, mitt-horisontell);
+					}else if (klossPosition == 2) {
+						testaOmRotation(mitt, mitt+1, mitt+2, mitt-1);
+					}else if (klossPosition == 3) {
+						testaOmRotation(mitt, mitt+horisontell, mitt+horisontell*2, mitt-horisontell);
+					}else if (klossPosition == 4) {
+						testaOmRotation(mitt, mitt+1, mitt+2, mitt-1);
+					};
+				}else if (bitTyp == 2) {
+					if (klossPosition == 1) {
+						stickUtVänster = 1;
+						stickUtHöger = 0;
+						stickUtUpp = 1;
+					}else if (klossPosition == 2) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 0;
+					}else if (klossPosition == 3) {
+						stickUtVänster = 0;
+						stickUtHöger = 1;
+						stickUtUpp = 1;
+					}else if (klossPosition == 4) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 0;
+					};
+					testaOmPush();
+					if (klossPosition == 1) {
+						testaOmRotation(mitt, mitt+horisontell, mitt-horisontell, mitt+horisontell-1);
+					}else if (klossPosition == 2) {
+						testaOmRotation(mitt, mitt+1, mitt-1, mitt-1-horisontell);
+					}else if (klossPosition == 3) {
+						testaOmRotation(mitt, mitt+horisontell, mitt-horisontell, mitt-horisontell+1);
+					}else if (klossPosition == 4) {
+						testaOmRotation(mitt, mitt-1, mitt+1, mitt+1+horisontell);
+					};
+				}else if (bitTyp == 3) {
+					if (klossPosition == 1) {
+						stickUtVänster = 1;
+						stickUtHöger = 0;
+						stickUtUpp = 1;
+					}else if (klossPosition == 2) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 1;
+					}else if (klossPosition == 3) {
+						stickUtVänster = 0;
+						stickUtHöger = 1;
+						stickUtUpp = 1;
+					}else if (klossPosition == 4) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 0;
+					};
+					testaOmPush();
+					if (klossPosition == 1) {
+						testaOmRotation(mitt, mitt+horisontell, mitt-horisontell, mitt-horisontell-1);
+					}else if (klossPosition == 2) {
+						testaOmRotation(mitt, mitt+1, mitt-1, mitt+1-horisontell);
+					}else if (klossPosition == 3) {
+						testaOmRotation(mitt, mitt+horisontell, mitt-horisontell, mitt+horisontell+1);
+					}else if (klossPosition == 4) {
+						testaOmRotation(mitt, mitt-1, mitt+1, mitt-1+horisontell);
+					};
+				}else if (bitTyp == 4) {
+
+				}else if (bitTyp == 5) {
+					if (klossPosition == 1) {
+						stickUtVänster = 1;
+						stickUtHöger = 0;
+						stickUtUpp = 1;
+					}else if (klossPosition == 2) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 1;
+					}else if (klossPosition == 3) {
+						stickUtVänster = 1;
+						stickUtHöger = 0;
+						stickUtUpp = 1;
+					}else if (klossPosition == 4) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 1;
+					};
+					testaOmPush();
+					if (klossPosition == 1) {
+						testaOmRotation(mitt, mitt-1, mitt+horisontell, mitt-horisontell-1);
+					}else if (klossPosition == 2) {
+						testaOmRotation(mitt, mitt+1, mitt+horisontell, mitt-1+horisontell);
+					}else if (klossPosition == 3) {
+						testaOmRotation(mitt, mitt-1, mitt+horisontell, mitt-horisontell-1);
+					}else if (klossPosition == 4) {
+						testaOmRotation(mitt, mitt+1, mitt+horisontell, mitt-1+horisontell);
+					};
+
+				}else if (bitTyp == 6) {
+					if (klossPosition == 1) {
+						stickUtVänster = 1;
+						stickUtHöger = 0;
+						stickUtUpp = 1;
+					}else if (klossPosition == 2) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 1;
+					}else if (klossPosition == 3) {
+						stickUtVänster = 0;
+						stickUtHöger = 1;
+						stickUtUpp = 1;
+					}else if (klossPosition == 4) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 0;
+					};
+					testaOmPush();
+					if (klossPosition == 1) {
+						testaOmRotation(mitt, mitt-1, mitt-horisontell, mitt+horisontell);
+					}else if (klossPosition == 2) {
+						testaOmRotation(mitt, mitt-1, mitt+1, mitt-horisontell);
+					}else if (klossPosition == 3) {
+						testaOmRotation(mitt, mitt+1, mitt-horisontell, mitt+horisontell);
+					}else if (klossPosition == 4) {
+						testaOmRotation(mitt, mitt+1, mitt-1, mitt+horisontell);
+					};
+				}else if (bitTyp == 7) {
+					if (klossPosition == 1) {
+						stickUtVänster = 1;
+						stickUtHöger = 0;
+						stickUtUpp = 1;
+					}else if (klossPosition == 2) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 0;
+					}else if (klossPosition == 3) {
+						stickUtVänster = 1;
+						stickUtHöger = 0;
+						stickUtUpp = 1;
+					}else if (klossPosition == 4) {
+						stickUtVänster = 1;
+						stickUtHöger = 1;
+						stickUtUpp = 0;
+					};
+					testaOmPush();
+					if (klossPosition == 1) {
+						testaOmRotation(mitt, mitt-1, mitt-horisontell, mitt+horisontell-1);
+					}else if (klossPosition == 2) {
+						testaOmRotation(mitt, mitt-1, mitt+horisontell, mitt+1+horisontell);
+					}else if (klossPosition == 3) {
+						testaOmRotation(mitt, mitt-1, mitt-horisontell, mitt+horisontell-1);
+					}else if (klossPosition == 4) {
+						testaOmRotation(mitt, mitt-1, mitt+horisontell, mitt+1+horisontell);
+					};
 				};
 			}else if (event.key == "s" || event.key == "S") {
 				var block2;
@@ -293,6 +513,8 @@ document.addEventListener("DOMContentLoaded",
 						lista[plats] = scroll;
 						plats++;
 					};	
+					mitt--;
+					horisontellPlacering--;
 				};
 				
 			}else if (event.key == "d" || event.key == "D") {
@@ -331,12 +553,38 @@ document.addEventListener("DOMContentLoaded",
 						lista[plats] = scroll;
 						plats++;
 					};
+					mitt++;
+					horisontellPlacering++;
 				};
 				
 
 			};
+			console.log("horisontellPlacering + " + horisontellPlacering);
+			console.log("vertikalPlacering + " + vertikalPlacering);
 			console.log(event.key + ": klossrörelse");
 			rita();
+		};
+
+		function testaOmRotation (a, b, c ,d) {
+			scroll = 1;
+			if (d+tryckHöger+horisontell*tryckNer <= horisontell*vertikal && c+tryckHöger+horisontell*tryckNer <= horisontell*vertikal && b+tryckHöger+horisontell*tryckNer <= horisontell*vertikal && a+tryckHöger+horisontell*tryckNer <= horisontell*vertikal && lista[a+tryckHöger+horisontell*tryckNer] != 2 && lista[a+tryckHöger+horisontell*tryckNer] != 3 && lista[b+tryckHöger+horisontell*tryckNer] != 2 && lista[b+tryckHöger+horisontell*tryckNer] != 3 && lista[c+tryckHöger+horisontell*tryckNer] != 2 && lista[c+tryckHöger+horisontell*tryckNer] != 3 && lista[d+tryckHöger+horisontell*tryckNer] != 2 && lista[d+tryckHöger+horisontell*tryckNer] != 3) {
+						while (scroll <= horisontell*vertikal) {
+							if (lista[scroll] == 1) {
+								lista[scroll] = 0;
+							};
+							scroll++;
+						};
+						lista[a+tryckHöger+horisontell*tryckNer] = 1;
+						lista[b+tryckHöger+horisontell*tryckNer] = 1;
+						lista[c+tryckHöger+horisontell*tryckNer] = 1;
+						lista[d+tryckHöger+horisontell*tryckNer] = 1;
+						if (klossPosition == 4) {
+							klossPosition = 1;
+						}else {
+							klossPosition++;
+						};
+						console.log("mitt = " + mitt);
+					};
 		};
 
 		//	gör så att skiten som är längst ner inte bara försvinner, utan fastnar
@@ -351,7 +599,7 @@ document.addEventListener("DOMContentLoaded",
 			};
 			placement = 1;
 			while (vandra >= 1) {
-				if (lista[vandra] == 1 && vandra > horisontell*vertikal-horisontell || lista[vandra+horisontell] == 2 && lista[vandra] == 1 || lista[vandra+horisontell] == 3	 && lista[vandra] == 1 || lista[vandra+1] == 2 && lista[vandra] == 1 || lista[vandra-1] == 2 && lista[vandra] == 1) {
+				if (lista[vandra] == 1 && vandra > horisontell*vertikal-horisontell || lista[vandra+horisontell] == 2 && lista[vandra] == 1 || lista[vandra+horisontell] == 3 && lista[vandra] == 1 || lista[vandra+1] == 2 && lista[vandra] == 1 || lista[vandra-1] == 2 && lista[vandra] == 1) {
 					lista[vandra] = 2;
 				};
 				vandra--;
@@ -377,21 +625,29 @@ document.addEventListener("DOMContentLoaded",
 			console.log("stuckPermanent");
 		};
 
+		function testaOmPush () {
+			frånVänsterKant = horisontellPlacering - 1;
+			frånHögerKant = horisontell - horisontellPlacering;
+			frånTopp = vertikalPlacering - 1;
+			if (stickUtVänster - frånVänsterKant > 0) {
+				tryckHöger = stickUtVänster - frånVänsterKant;
+			}else if (stickUtHöger - frånHögerKant > 0) {
+				tryckHöger = frånHögerKant - stickUtHöger;
+			}else {
+				tryckHöger = 0;
+			};
+			if (stickUtUpp - frånTopp > 0) {
+				tryckNer = stickUtUpp - frånTopp;
+			}else {
+				tryckNer = 0;
+			};
+		};
+
 		styleSetup();		//stilfixarn
 		tetrisskapare(horisontell, vertikal); //divboxar, spelplan
 		skapaLista(horisontell, vertikal); // lista                        Dessa körs efterdirekt DOMContentLoaded och behöver bara köras en gång.
 
-
-
-
-
-		// runttramsande för testning av funktion
-		// lista[1] = 0;
-		// lista[12] = 2;
-		// lista[13] = 2;
-		// lista[14] = 2;
-
-			var milliPerTick = 1000;
+			var milliPerTick = 700;
 			variabeln = setInterval(webTetris, milliPerTick);
 			function webTetris() {
 					görStuck();
